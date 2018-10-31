a7a7ab5451ba292d9fdeb73326a541d692511431
506017395 <506017395@qq.com>
2018-10-31 19:19:20 +0800

    create AXFProject

---
+++ axf/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from axf import views
+
+urlpatterns = [
+    url("^$", views.home, name="home"),  # 首页
+    url("^market/$", views.market, name="market"),  # 闪购超市
+    url("^cart/$", views.cart, name="cart"),  # 购物车
+    url("^mine/$", views.mine, name="mine"),  # 我的
+]
---
+++ axf/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+# 首页
+def home(request):
+    return render(request, "home/home.html")
+
+
+# 闪购超市
+def market(request):
+    return render(request, "market/market.html")
+
+
+# 购物车
+def cart(request):
+    return render(request, "cart/cart.html")
+
+
+# 我的
+def mine(request):
+    return render(request, "mine/mine.html")
