2d6ecbaba1071ebb07d5c117d553888edc8f1b61
506017395 <506017395@qq.com>
2018-11-02 16:50:44 +0800

    complete HomePage

--- axf/models.py
+++ axf/models.py
@@ -28,3 +28,42 @@ class Nav(Base):
 class Mustbuy(Base):
     class Meta:
         db_table = "axf_mustbuy"
+
+
+# 商品部分
+class Shop(Base):
+    class Meta:
+        db_table = "axf_shop"
+
+
+# 主体内容
+class Mainshow(models.Model):
+    trackid = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=20)
+    brandname = models.CharField(max_length=20)
+
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=20)
+    productid1 = models.CharField(max_length=20)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.DecimalField(max_digits=10, decimal_places=2)
+    marketprice1 = models.DecimalField(max_digits=10, decimal_places=2)
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=20)
+    productid2 = models.CharField(max_length=20)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.DecimalField(max_digits=10, decimal_places=2)
+    marketprice2 = models.DecimalField(max_digits=10, decimal_places=2)
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=20)
+    productid3 = models.CharField(max_length=20)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.DecimalField(max_digits=10, decimal_places=2)
+    marketprice3 = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = "axf_mainshow"
--- axf/views.py
+++ axf/views.py
@@ -2,19 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 # 首页
-from axf.models import Wheel, Nav, Mustbuy
+from axf.models import Wheel, Nav, Mustbuy, Shop, Mainshow
 
 
 def home(request):
     # 获取轮播数据
     wheels = Wheel.objects.all()
+    # 获取导航数据
     navs = Nav.objects.all()
+    # 获取闪购轮播
     mustbuys = Mustbuy.objects.all()
-    print(len(mustbuys))
+    # 获取商品
+    shop = Shop.objects.all()
+    shophead, shoptabs, shopclasss, shopcommends = shop[0], shop[1:3], shop[3:7], shop[7:]
+    # 主体部分
+    mainshows = Mainshow.objects.all()
+
     result = {
         "wheels": wheels,
         "navs": navs,
         "mustbuys": mustbuys,
+        "shophead": shophead,
+        "shoptabs": shoptabs,
+        "shopclasss": shopclasss,
+        "shopcommends": shopcommends,
+        "mainshows": mainshows,
     }
     return render(request, "home/home.html", context=result)
 
